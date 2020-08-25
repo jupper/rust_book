8c77ba773bc8bd8377622f9368930d0a27378165
Valentin Olchowski <valentin.olchowski@gmail.com>
2020-08-25 19:36:19 +0200

    Chapter 5

---
+++ rectangles/target/debug/deps/rectangles-e09557f54081036f.d
@@ -0,0 +1,5 @@
+/home/valentin/Programming/Rust/rust_book/rectangles/target/debug/deps/rectangles-e09557f54081036f: src/main.rs
+
+/home/valentin/Programming/Rust/rust_book/rectangles/target/debug/deps/rectangles-e09557f54081036f.d: src/main.rs
+
+src/main.rs:
---
+++ rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+/home/valentin/Programming/Rust/rust_book/rectangles/target/debug/rectangles: /home/valentin/Programming/Rust/rust_book/rectangles/src/main.rs
